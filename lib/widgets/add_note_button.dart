1e509f2a9ef94bc089be63eae1e4a717da72a079
Hassan_Ali <hassanelsherif90@gmail.com>
2024-10-01 15:51:14 +0300

    modified:   lib/widgets/add_note_button.dart 	new file:   lib/widgets/custom_button.dart 	modified:   lib/widgets/custom_text_field.dart

--- lib/widgets/add_note_button.dart
+++ lib/widgets/add_note_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
@@ -10,13 +12,31 @@ class AddNoteButtonSheet extends StatelessWidget {
       padding: EdgeInsets.symmetric(
         horizontal: 8,
       ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 50,
-          ),
-          CustomTextField(),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            CustomTextField(
+              hint: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              maxLines: 5,
+              hint: 'content',
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
