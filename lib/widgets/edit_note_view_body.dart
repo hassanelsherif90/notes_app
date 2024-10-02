a3ca4add0eeded3da1e9fa02a63a056b10cfef6e
Hassan_Ali <hassanelsherif90@gmail.com>
2024-10-02 08:49:31 +0300

    craete NoteModel

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -7,14 +8,15 @@ class EditNoteViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 18),
+      padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
+          SizedBox(height: 60),
+          CustomAppBar(title: 'Edit', icon: Icons.check),
           SizedBox(height: 50),
-          CustomAppBar(
-            title: 'Edit',
-            icon: Icons.check,
-          ),
+          CustomTextField(hint: 'Title'),
+          SizedBox(height: 15),
+          CustomTextField(hint: 'Content', maxLines: 5),
         ],
       ),
     );
